16b29bbd0a753e521279705125a0bfcbf42f8d4a
trickstarcandina <rinnegan101220@gmail.com>
2022-05-19 10:24:11 +0700

    init: init project

---
+++ src/main/java/com/example/elasticsearchrestapi/ElasticsearchRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearchrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchRestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/elasticsearchrestapi/ElasticsearchRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearchrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
